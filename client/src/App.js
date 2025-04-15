c57b2eb2ae2bc1c5b7983555fd36341e10f2050a
Tomer Gueta <tgtelhai@gmail.com>
2025-04-15 11:53:50 +0300

    Add initial React app with MUI installed and test button

---
+++ client/src/App.js
@@ -0,0 +1,15 @@
+import React from "react";
+import Button from "@mui/material/Button";
+
+function App() {
+  return (
+    <div style={{ padding: "2rem" }}>
+      <h1>Hello Tomer 👋</h1>
+      <Button variant="contained" color="primary">
+        I'm a MUI Button!
+      </Button>
+    </div>
+  );
+}
+
+export default App;
